a96103b3261e6f1363686aa9758f3345388ca4f4
LIS <laboratoriois@udea.edu.co>
2019-08-17 11:37:19 -0500

    Servicios creados en la clase 4

---
+++ RestAPI/src/main/java/com/microservicios2/clase4/restapi/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.microservicios2.clase4.restapi.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.microservicios2.clase4.restapi.model.Student;
+
+public interface StudentRepository extends MongoRepository<Student, String>{
+
+}
